e61b3f9a87a3d67b16b2e2f9397d4c522ba4dd3b
Gabriella Angelina Widjaja <162145755+gabriellaawidd@users.noreply.github.com>
2026-08-05 11:44:13 +0700

    Initial Commit

---
+++ SonAR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SonAR
+//
+//  Created by Gabriella Angelina Widjaja on 05/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SonAR/SonARApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonARApp.swift
+//  SonAR
+//
+//  Created by Gabriella Angelina Widjaja on 05/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SonARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
